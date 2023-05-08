6409e5e90cc835ef616db0d5cd07c52a800ac8e7
students <alkobakalova@beleriand.velkerr.ru>
2023-05-08 06:34:30 +0000

    add import script

---
+++ import-script.py
@@ -0,0 +1,7 @@
+from nlgeval.metrics.mover_score import MoverScoreMetrics
+from nlgeval.metrics.blanc import BlancMetrics
+from nlgeval.metrics.ngram import BLEUMetrics, METEORMetrics, ROUGEMetrics, CHRFMetrics
+from nlgeval.metrics.pretrained import BERTScoreMetrics, BARTScoreMetrics
+from nlgeval.metrics.reference_free import CompressionMetrics, CoverageMetrics, LengthMetrics, NoveltyMetrics, DensityMetrics, RepetitionMetrics
+from nlgeval.metrics.rouge_we import RougeWeMetrics
+from nlgeval.metrics.s3 import S3Metrics
